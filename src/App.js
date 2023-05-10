f42b486a4d1f0bb0ed5c101e4b69008d17443a75
Mahip Adhikari <mahip.adhikari4@gmail.com>
2023-05-10 22:25:21 +0545

    ready for deployment

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App = () => <div>Hello from ashraf kabir</div>;
+
+export default App;
+ 
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API = "https://ecommerce-bun6.onrender.com/api/"
