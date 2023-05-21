e45fdd3b1a5ce9206a18f5f5c7da1bd0c9ef8c7a
Mahip Adhikari <mahip.adhikari4@gmail.com>
2023-05-21 19:53:51 +0545

    copyright edited

--- src/core/Copyright.js
+++ src/core/Copyright.js
@@ -9,7 +9,7 @@ export default function Copyright() {
       <Typography variant='body2' color='textSecondary' align='center'>
         {'Copyright © '}
         <Link color='inherit' href='#'>
-          Mahip Adhikari
+        Basanta Bohara
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
